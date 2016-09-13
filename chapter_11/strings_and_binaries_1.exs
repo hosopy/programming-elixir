4a38f7f92359a5883a9a93590ec8a4e6a0fc862a
hosopy <dev.hosopy@gmail.com>
2016-09-13 20:41:04 +0900

    tweaks

--- chapter_11/strings_and_binaries_1.exs
+++ chapter_11/strings_and_binaries_1.exs
@@ -1,5 +1,5 @@
 defmodule MyString do
   def has_ascii?(char_list) when is_list(char_list) do
-    Enum.any? char_list, fn x -> x >= 32 and x <= 126 end
+    Enum.any? char_list, fn x -> x >= ?\s and x <= ?~ end
   end
 end
