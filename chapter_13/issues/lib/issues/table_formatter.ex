d7b6cf89244644a9dffde2cbbe129b5fcb783c3a
hosopy <dev.hosopy@gmail.com>
2016-09-23 18:31:32 +0900

    trial of DocTest

--- chapter_13/issues/lib/issues/table_formatter.ex
+++ chapter_13/issues/lib/issues/table_formatter.ex
@@ -12,6 +12,19 @@ defmodule Issues.TableFormatter do
     end
   end
 
+  @doc"""
+  Given a list of rows, where each row contains a keyed list
+  of columns, return a list containing lists of the data in
+  each column. The `headers` parameter contains the list of
+  columns to extract
+
+  ## Example
+
+      iex> list = [Enum.into([{"a", "1"},{"b", "2"},{"c", "3"}], %{}),
+      ...>         Enum.into([{"a", "4"},{"b", "5"},{"c", "6"}], %{})]
+      iex> Issues.TableFormatter.split_into_columns(list, ["a", "b", "c"])
+      [ ["1", "4"], ["2", "5"], ["3", "6"] ]
+  """
   def split_into_columns(rows, headers) do
     for header <- headers do
       for row <- rows, do: printable(row[header])
