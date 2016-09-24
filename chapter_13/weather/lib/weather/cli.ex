e463e3c953be4280524a9a0da97bb35c4d327b0c
hosopy <dev.hosopy@gmail.com>
2016-09-24 12:46:23 +0900

    Add escript entrypoint

---
+++ chapter_13/weather/lib/weather/cli.ex
@@ -0,0 +1,11 @@
+defmodule Weather.CLI do
+
+  @moduledoc """
+  Fetche weather data from http://w1.weather.gov/xml/current_obs/KDTO.xml
+  and print formatted content.
+  """
+
+  def main(argv) do
+    # TODO implement
+  end
+end
